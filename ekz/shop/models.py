65b6f33339cf02a8bda4756764070792a4785e23
s3kken <your.unicorn0@gmail.com>
2023-01-28 21:54:11 +0700

    cоня приветик2

--- ekz/shop/models.py
+++ ekz/shop/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    avatar = models.ImageField(blank=False, upload_to="images/avatar", verbose_name="avatar")
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='media/')
+
+    def __str__(self):
+        return self.name
--- ekz/shop/views.py
+++ ekz/shop/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from msilib.schema import ListView
 
-# Create your views here.
+import generic as generic
+from django.views.generic.base import TemplateView
+from django.contrib.auth.views import LoginView
+from ekz.shop.models import Product, User
+from django.views.generic.edit import CreateView
+
+
+class Index(TemplateView):
+    template_name = 'index.html'
+
+class Products(ListView):
+    model = Product
+    template_name = 'product_list.html'
+    context_object_name = 'products'
+
+class Login(LoginView):
+    template_name = 'login.html'
+    model = User
+    success_url = '/'
+
+class RegisterUser(CreateView):
+    form_class = Registration
+    template_name = 'registration.html'
+    success_url = reverse_lazy('login')
+    success_page = 'registration'
+
+class Profile(generic.ListView):
+    model = User
+    template_name = 'profile.html'
+
+class DetailView(generic.DetailView):
+    model = Product
+    template_name = 'product.html'
+
+class Order(TemplateView):
+    template_name = 'order.html'
